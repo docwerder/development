62dc94325c6e5097b2c353809764399c6625e573
docwerder <wingbermuehle@icloud.com>
2022-02-15 19:02:10 +0100

    V1.01 minor modifications

--- progressbar_QThread.py
+++ progressbar_QThread.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QProgressBar, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QLabel, QDialog, QProgressBar, QVBoxLayout, QPushButton
 import sys
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
@@ -11,14 +11,14 @@ class MyThread(QThread):
         cnt = 0
         while cnt < 100:
             cnt += 1
-            time.sleep(0.5)
+            time.sleep(2)
             self.change_value.emit(cnt)
 
-    def run(self):
+    def run(self) -> None:
         cnt = 0
         while cnt < 100:
             cnt += 1
-            time.sleep(0.1)
+            time.sleep(2)
             self.change_value.emit(cnt)
 
 class Window(QDialog):
@@ -35,26 +35,51 @@ class Window(QDialog):
         self.setWindowIcon(QtGui.QIcon(self.iconName))
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
+        self.clicked_value = 0
         self.InitUI()
 
     def InitUI(self):
+        # General layout
         vbox = QVBoxLayout()
+
+        # Progressbar...
         self.progressbar = QProgressBar()
         self.progressbar.adjustSize()
-        vbox.addWidget(self.progressbar)
+        self.progressbar.setStyleSheet("QProgressBar::chunk {background-color: #2196F3; width: 10px; margin: 0.5px;}")
 
+        # Button...
         self.button = QPushButton("Run Progressbar")
         self.button.clicked.connect(self.startProgressBar)
+
+        # QLabel...
+        self.clicksLabel = QLabel("Counting: 0 clicks", self)
+        self.clicksLabel.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+        # Button for clicking...
+        self.countBtn = QPushButton("Klick mich!", self)
+        self.countBtn.clicked.connect(self.countClicks)
+
+
+        # Add the widgets to the layout!
+        vbox.addWidget(self.progressbar)
         vbox.addWidget(self.button)
+        vbox.addWidget(self.clicksLabel)
+        vbox.addWidget(self.countBtn)
+
         self.setLayout(vbox)
 
     def startProgressBar(self):
         self.thread = MyThread()
-        self.thread.change_value.connect(self.setProgressVal)
+        #self.thread.change_value.connect(self.setProgressVal)
         self.thread.start()
 
     def setProgressVal(self, val):
         self.progressbar.setValue(val)
+
+    def countClicks(self):
+        self.clicked_value +=1
+        self.clicksLabel.setText(f"Counting: {self.clicked_value} clicks")
+        print('Klick mich clicked!')
 
 
 
